f7fe7d797d612d28826286ad0446ba971b8b74c8
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:28:59 +0300

    Create dikdortalan_fonk.py

---
+++ MY_EXERCISE/dikdortalan_fonk.py
@@ -0,0 +1,9 @@
+# Fonksiyon kullanarak genişliği ve yüksekliği girilen dikdörtgenin alanını hesaplayan Python programı
+
+def dikdurtgen(x,h):
+    return x*h
+
+x=int(input("genişlik: "))
+h=int(input("yükseklik: "))
+
+print(dikdurtgen(x,h))
