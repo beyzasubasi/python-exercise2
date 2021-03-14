8192e68a56601e7a8024eb0128645d933251231d
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:28:17 +0300

    Create 1den_n_ye_kadar_sayilar.py

---
+++ MY_EXERCISE/1den_n_ye_kadar_sayilar.py
@@ -0,0 +1,11 @@
+# 1 den N e kadar olan sayıları yazdıran algo
+
+i=1
+n = int(input("N değeri giriniz: "))
+
+#for i in range(1,n):    # for kullanarak yazdım
+#   print(i)
+
+while(i<n):              # while kullanarak yazdım
+    print(i)
+    i += 1
