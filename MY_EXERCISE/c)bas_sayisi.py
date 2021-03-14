7f33cb1a81f1acd153a6860c7c3e9e24df0cfeaf
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:28:46 +0300

    Create c)bas_sayisi.py

---
+++ MY_EXERCISE/c)bas_sayisi.py
@@ -0,0 +1,8 @@
+#c)	Verilen sayıdaki basamak sayısını hesaplayıp ekrana yazan bir program yazınız.
+#   Örneğin, 75869 sayısı için sonuç 5 olacaktır.
+
+x=str(input("Bir sayı giriniz: "))
+a=len(x)
+print(a)
+
+
