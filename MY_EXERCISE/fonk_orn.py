f1f150e34d9b380a9d1f819eae953ccfb2ee2234
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:29:20 +0300

    Create fonk_orn.py

---
+++ MY_EXERCISE/fonk_orn.py
@@ -0,0 +1,20 @@
+def hello():
+    print("Merhaba")
+
+hello()
+
+
+
+
+def hello(name):
+    print("Merhaba "+ name)
+
+hello("Arda")
+
+
+
+
+def hello(name):
+    return "Merhaba "+ name
+
+print(hello("Arda"))
