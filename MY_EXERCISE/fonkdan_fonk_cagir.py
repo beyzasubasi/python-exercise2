24cc4c7cf93d3eada722c4b8a16d8d8fefd4196b
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:29:22 +0300

    Create fonkdan_fonk_cagir.py

---
+++ MY_EXERCISE/fonkdan_fonk_cagir.py
@@ -0,0 +1,9 @@
+#Gördüğünüz gibi 3 kişi için yaş hesaplama işlemini tek bir yasHesapla fonksiyonu ile yapabildik.
+
+def yas_hesapla(dogum_yili):
+    return 2020-dogum_yili
+
+yas_ada=yas_hesapla(2000)
+yas_arda=yas_hesapla(1998)
+
+print(yas_ada,yas_arda)
