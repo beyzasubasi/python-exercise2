297fae5e18ba390d31f9f6a74d275e7685314da1
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:31:43 +0300

    Create 03.py

---
+++ MY_EXERCISE2/03.py
@@ -0,0 +1,9 @@
+sayi1 = int(input("bir sayı girin: "))
+sayi2 = int(input("ikinci bir sayı girin: "))
+sayi3 = int(input("üçüncü bir sayı girin: "))
+ebs = sayi1
+if(sayi2>ebs):
+    ebs = sayi2
+if(sayi3>ebs):
+    ebs = sayi3
+print(ebs)
