f586a9ba820d6c24396bf4fc3e1422a9269189d2
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:28:49 +0300

    Create carpma_toplama.py

---
+++ MY_EXERCISE/carpma_toplama.py
@@ -0,0 +1,10 @@
+# klavyeden girilen 2 sayıdan 1.si büyükse çarpma, değilse toplama yapan algo
+
+x = int(input("İlk Sayıyı Giriniz: "))
+y = int(input("İkinci Sayiyi Giriniz: "))
+
+if(x>y):
+    print("Çarpımları :", x*y )
+
+else:
+    print("Toplamları: ", x+y)
