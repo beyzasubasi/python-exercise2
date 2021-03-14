2e4784ea73ecee8a5352658dc49599fc854d9445
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 14:29:40 +0300

    Create karealan_fonk.py

---
+++ MY_EXERCISE/karealan_fonk.py
@@ -0,0 +1,8 @@
+#Kenar uzunluğu verilen bir karenin alanını bulan alan adında bir fonksiyon yazalım.
+#Bu fonksiyonumuz alan değerini bize geri döndürsün(return).
+
+def kalan(kenar):
+    return kenar*kenar
+
+
+print(kalan(5))
